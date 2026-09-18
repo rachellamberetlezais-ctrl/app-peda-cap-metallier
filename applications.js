1be08b971a0eb676d331cbcfea4a8fc9bd5af60d
rachellamberetlezais-ctrl <rachellamberetlezais@gmail.com>
2026-09-18 15:31:41 +0400

    Update applications.js
    ajout de deux applications A1 S2 M1 et M2

--- applications.js
+++ applications.js
@@ -10,4 +10,25 @@
  *   lien: './applications/A1S3M1/index.html'
  * },
  */
-window.APPLICATIONS = [];
+window.APPLICATIONS = [const applications = [
+  {
+    code: 'A1S2M1',
+    annee: 1,
+    semaine: 2,
+    discipline: 'M',
+    activite: 1,
+    titre: 'Statistique : fréquences & diagrammes en bâtons',
+    description: 'Calculer des fréquences et représenter des données par un diagramme en bâtons à partir de situations professionnelles de métallerie.',
+    lien: './applications/A1S2M1/index.html'
+  },
+  {
+    code: 'A1S2M2',
+    annee: 1,
+    semaine: 2,
+    discipline: 'M',
+    activite: 2,
+    titre: 'Calculs numériques & puissances de 10',
+    description: 'Utiliser les puissances de 10, la notation scientifique et les ordres de grandeur dans des situations de mesure et de contrôle en métallerie.',
+    lien: './applications/A1S2M2/index.html'
+  }
+];];
